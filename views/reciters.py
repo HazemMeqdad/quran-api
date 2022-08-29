e6e68778592120d549e753fdeeeff5b11cc6a500
HazemMeqdad <hazemmeqdad@gmail.com>
2022-08-29 14:17:29 +0300

    Fix a url for reciters

--- views/reciters.py
+++ views/reciters.py
@@ -23,12 +23,12 @@ def index():
 def reader(path):
     file = os.path.exists("cdn/reciters/" + path)
     domain = app.config["DOMAIN"]
+    folder_name = path.split("/")[-1]
     if not file:
         return jsonify({"error": "reciter not found"}), 404
     if not path.endswith(".mp3"):
-        files = [domain + f"/reciters/{i}" for i in os.listdir("cdn/reciters/" + path)]
+        files = [domain + f"/reciters/{folder_name}/{i}" for i in os.listdir("cdn/reciters/" + path)]
         files.sort()
-        folder_name = path.split("/")[-1]
         with open("data.json", "r", encoding="utf-8") as f:
             reciters = json.load(f)["reciters"]
         return jsonify({"available_files": files, "name": reciters.get(folder_name, None) or folder_name})
